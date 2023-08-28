3661969178204619ed15b9588135f063691651e4
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-08-28 20:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let inputs=document.getElementsByTagName('input');
+let submitButton=document.getElementById('btn');
+let output=document.getElementById('output');
+
+submitButton.addEventListener('click',(event)=>{event.preventDefault();
+setTimeout(()=>{
+			output.innerHTML=inputs[0].value;
+			
+		},inputs[1].value)
+		
+										
+});
+
+
+
