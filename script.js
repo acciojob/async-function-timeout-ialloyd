2c135934745b9c2e2c0e39214699a651a2816464
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-08-29 09:32:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ let inputs=document.getElementsByTagName('input');
 let submitButton=document.getElementById('btn');
 let output=document.getElementById('output');
 
-submitButton.addEventListener('click',(event)=>{event.preventDefault();
+submitButton.addEventListener('click',(event)=>{
+	
+	event.preventDefault();
+	func();										
+});
+
+
+async function func(){
+
+	await new Promise((resolve)=>{
+
+	
 setTimeout(()=>{
+
+			resolve();
 			output.innerHTML=inputs[0].value;
 			
 		},inputs[1].value)
 		
-										
-});
+	})
+}
 
 
 
